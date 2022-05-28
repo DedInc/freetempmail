f6cb26f226b1962d99dc0d62400e8ff30a798c1c
DedInc <visitanimation@gmail.com>
2022-05-28 10:52:11 +0600

    some fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='freetempmail',
-    version='1.0.0',
+    version='1.0.1',
     author='Maehdakvan',
     author_email='visitanimation@google.com',
     description='Temp-Mail.org API wrapper.',
